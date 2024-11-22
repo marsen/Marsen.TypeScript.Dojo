fabf1b0d0b28a498a301a705c889b61c7dfaa1c1
marsen <admin@marsen.me>
2024-11-23 03:22:20 +0800

    test: 更新計算器測試用例，將 Mocha 的 expect 語法轉換為 Jest 的 toBe 方法

--- tests/Calculator/Calculator.test.ts
+++ tests/Calculator/Calculator.test.ts
@@ -3,12 +3,11 @@ import C from '../../src/Calculator/Calculator';
 describe('calculate', function() {
   it('add', function() {
     const result = C.Sum(5, 2);
-    expect
-    (result).equal(7);
+    expect(result).toBe(7);
   });
 
   it('substract', function() {
     const result = C.Difference(5, 2);
-    expect(result).equal(3);
+    expect(result).toBe(3);
   });
 });
