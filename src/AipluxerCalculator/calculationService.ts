94ca231994808bc7b4bdb551f3c082bc4c87570a
marsen <admin@marsen.me>
2024-11-25 16:31:33 +0800

    refactor: 將 createItemDetail 方法的調用替換為內聯物件，簡化 itemsDetail 的定義

--- src/AipluxerCalculator/calculationService.ts
+++ src/AipluxerCalculator/calculationService.ts
@@ -80,7 +80,7 @@ export class CalculationService implements ICalculationService {
       const itemsDetail: ItemsDetail[] = [
         { name: PRODUCT_NAMES[0], quantity: 1, unitPrice: this.governmentFee, totalPrice: this.governmentFee },
         { name: PRODUCT_NAMES[1], quantity: planType === 'basic' ? 1 : 0, unitPrice: planType === 'basic' ? this.basicFee : 0, totalPrice: planType === 'basic' ? this.basicFee : 0 },
-        this.createItemDetail(PRODUCT_NAMES[2], planType === 'advanced' ? 1 : 0, planType === 'advanced' ? this.advancedFee : 0, planType === 'advanced' ? this.advancedFee : 0),
+        { name: PRODUCT_NAMES[2], quantity: planType === 'advanced' ? 1 : 0, unitPrice: planType === 'advanced' ? this.advancedFee : 0, totalPrice: planType === 'advanced' ? this.advancedFee : 0 },
         this.createItemDetail(PRODUCT_NAMES[3], code >= '01' && code <= '34' ? excessQty : 0, code >= '01' && code <= '34' && excessQty !== 0 ? this.goodsFee : 0, code >= '01' && code <= '34' ? excessTotalFee : 0),
         this.createItemDetail(PRODUCT_NAMES[4], code === '35' && items.some(item => item.startsWith('3519')) ? excessQty : 0, code === '35' && items.some(item => item.startsWith('3519')) && excessQty !== 0 ? this.specialServiceFee : 0, code === '35' && items.some(item => item.startsWith('3519')) ? excessTotalFee : 0)
       ]
