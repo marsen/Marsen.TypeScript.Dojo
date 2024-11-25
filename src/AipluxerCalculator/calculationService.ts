6ccfd9bea7dc73e16a8d577380d44d6a69f10581
marsen <admin@marsen.me>
2024-11-25 20:05:38 +0800

    refactor: 使用 ProductItem 類別重構計算服務中的額外費用項目以提升可讀性

--- src/AipluxerCalculator/calculationService.ts
+++ src/AipluxerCalculator/calculationService.ts
@@ -109,18 +109,8 @@ export class CalculationService implements ICalculationService {
         new ProductItem('application_regulation_fee', 1, this.governmentFee),
         new ProductItem('application_basic_service_fee', (isBasic(planType) ? 1 : 0), (isBasic(planType) ? this.planFeeDic[planType] : 0)),
         new ProductItem('application_advanced_service_fee', (!isBasic(planType) ? 1 : 0), (!isBasic(planType) ? this.planFeeDic[planType] : 0)),
-        {
-          name: PRODUCT_NAMES[3],
-          quantity: this.isGoods(c.code) ? excessQty : 0,
-          unitPrice: this.isGoods(c.code) && excessQty !== 0 ? this.goodsFee : 0,
-          totalPrice: this.isGoods(c.code) ? excessTotalFee : 0
-        },
-        {
-          name: PRODUCT_NAMES[4],
-          quantity: this.isSpecial(c.items) ? excessQty : 0,
-          unitPrice: this.isSpecial(c.items) && excessQty !== 0 ? this.specialServiceFee : 0,
-          totalPrice: this.isSpecial(c.items) ? excessTotalFee : 0
-        }
+        new ProductItem('excess_item_fee', (this.isGoods(c.code) ? excessQty : 0), (this.isGoods(c.code) && excessQty !== 0 ? this.goodsFee : 0)),
+        new ProductItem('special_excess_item_fee', (this.isSpecial(c.items) ? excessQty : 0), (this.isSpecial(c.items) && excessQty !== 0 ? this.specialServiceFee : 0))
       ]
       return { code: c.code, codeQty, excessTotalFee, fee, subTotal, items, excessFee, excessQty }
 
