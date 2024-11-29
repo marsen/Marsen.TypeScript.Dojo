65f87f7161f6af5f62ee56b50ae65e02ffd489ac
marsen <admin@marsen.me>
2024-11-29 18:12:25 +0800

    test: 新增測試案例以驗證計算服務的超項處理邏輯，並重構測試資料生成方式

--- tests/AipluxerCalculator/calculationService.test.ts
+++ tests/AipluxerCalculator/calculationService.test.ts
@@ -6,7 +6,8 @@ describe('CalculationService', () => {
   beforeEach(() => {
     target = new CalculationService()
   })
-  
+  const mockCategoryItems = (code:string,Qty:number)=> 
+    [{ code:code.substring(0,2), items: Array.from({ length: Qty }, (_, i) => `${code}${(i + 1).toString().padStart(2, '0')}`) }] 
   describe('createItemDetail', () => {
     it('一般方案，無超項(01類-2項)', async () => {
       //arrange
@@ -51,10 +52,59 @@ describe('CalculationService', () => {
         "subtotal": 4600,
         "total": 4600,
       }
+      
       //act
-      const result = await target.calculateCategoriesFeeDetail('basic', [{ code: '01', items: ['0101', '0102'] }], false)
+      const result = await target.calculateCategoriesFeeDetail('basic', mockCategoryItems('01',2), false)
       //assert
       expect(result).toEqual(expected)
+    })
+    it('一般方案，有超項(08類-28項)', async () => {
+      //arrange
+      const expected =  {
+        "categoriesPriceDetail": [{
+          "code": "08",
+          "codeQty": 28,
+          "excessFee": 200,
+          "excessQty": 8,
+          "excessTotalFee": 1600,
+          "fee": 0,
+          "items":[{
+              "name": "application_regulation_fee",
+              "quantity": 1,
+              "totalPrice": 2400,
+              "unitPrice": 2400,
+            },{
+              "name": "application_basic_service_fee",
+              "quantity": 1,
+              "totalPrice": 2200,
+              "unitPrice": 2200,
+            },{
+              "name": "application_advanced_service_fee",
+              "quantity": 0,
+              "totalPrice": 0,
+              "unitPrice": 0,
+            },{
+              "name": "excess_item_fee",
+              "quantity": 8,
+              "totalPrice": 1600,
+              "unitPrice": 200,
+            },{
+              "name": "special_excess_item_fee",
+              "quantity": 0,
+              "totalPrice": 0,
+              "unitPrice": 0,
+            }],            
+          "subTotal": 6200,  
+        }],
+        "excessFee": 1600,
+        "planFee": 4600,
+        "subtotal": 6200,
+        "total": 6200,
+      }
+      //act
+      const result = await target.calculateCategoriesFeeDetail('basic', mockCategoryItems('08',28), false)
+      //assert
+      expect(result).toMatchObject(expected)
     })
     
     it('進階方案，無超項(01類-2項)', async () => {
@@ -101,7 +151,7 @@ describe('CalculationService', () => {
         "total": 10000,
       }
       //act
-      const result = await target.calculateCategoriesFeeDetail('advanced', [{ code: '01', items: ['0101', '0102'] }], false)
+      const result = await target.calculateCategoriesFeeDetail('advanced', mockCategoryItems('01',2), false)
       //assert
       expect(result).toMatchObject(expected)
     })
